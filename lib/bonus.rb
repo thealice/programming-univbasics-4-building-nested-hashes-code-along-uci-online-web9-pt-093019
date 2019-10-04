913220805d8a8f2533c372006648c14328f764a9
Alice Freda <alicefreda@gmail.com>
2019-10-04 19:13:58 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,12 +20,7 @@ def bonus
    }
   }
   epic_tragedy[:montague][:hero][:status] = "dead"
-
-  puts epic_tragedy
-
   epic_tragedy[:capulet][:heroine][:status] = "dead"
-
-  puts epic_tragedy
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
